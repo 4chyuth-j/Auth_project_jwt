3f3c713a97e151616af0fd0a1d1fa64c3ecccf9a
4chyuth-j <achyuthj2002@gmail.com>
2025-10-31 11:55:30 +0530

    revised index.js

---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
+import authRoutes from './routes/auth.route.js';
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000 ;
+
+app.use(express.json()); //used to parse the incoming data to req.body
+
+
+
+app.use('/api/auth',authRoutes);
+
+//only run the application after connecting to the database
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on : http://localhost:${PORT}`);
+    })
+})
+
+
+
+
+
+
+
+
+
+
